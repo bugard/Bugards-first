50246aad24bdadbbfc435048e748d871638c034b
Bugard <Bugard.liddell@yahoo.com>
2016-01-28 17:05:07 -0500

    added java hello world
    trying to see if i can add the whole folder of hello world from java to
    github folder instead of just a text file

---
+++ 82614_HelloWorld/src/HelloWorld.java
@@ -0,0 +1,17 @@
+import javax.swing.JOptionPane;
+
+/*Bugard Liddell
+	 * Input: none
+	 * algorithm: none
+	 * output: prints hello world
+	 * @param args
+	 */
+public class HelloWorld {
+
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		JOptionPane.showMessageDialog(null,
+		"Hello World", "Java Day One", JOptionPane.INFORMATION_MESSAGE);
+	}
+}
